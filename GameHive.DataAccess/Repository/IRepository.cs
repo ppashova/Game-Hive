fe926e803d63db53d3ee06e8d549266ec92a32ff
Pashova <ppashova06@gmail.com>
2025-01-24 09:10:49 +0200

    Added IRepository and Repository in Data Access

---
+++ GameHive.DataAccess/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHive.DataAccess.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        T Get(int id);
+        List<T> GetAll();
+        List<T> Find(Expression<Func<T, bool>> filter);
+        List<T> CheckIfExists(List<int> id);
+    }
+}
---
+++ GameHive.DataAccess/Repository/Repository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameHive.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        internal Microsoft.EntityFrameworkCore.DbSet<T> dbSet;
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            this.dbSet = _context.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            T obj = dbSet.Find(id);
+            dbSet.Remove(obj);
+        }
+
+        public List<T> Find(Expression<Func<T, bool>> filter)
+        {
+            return dbSet.Where(filter).ToList();
+        }
+
+        public T Get(int id)
+        {
+            T obj = dbSet.Find(id);
+            return obj;
+        }
+
+        public List<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+
+        public void Update(T entity)
+        {
+            dbSet.Update(entity);
+        }
+    }
+}
