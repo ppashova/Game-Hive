19f21c43a6b92ecea9b246dea1ea736d45c881c0
ppashova <ppashova06@gmail.com>
2025-01-26 23:42:05 +0200

    Added Tag Services and Repository and Service injections

---
+++ GameHive.Core/IServices/ITagService.cs
@@ -0,0 +1,20 @@
+﻿using GameHive.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHive.Core.IServices
+{
+    public interface ITagService
+    {
+        Tag GetById(int id);
+        void Add(Tag tag);
+        void Update(Tag tag);
+        public void Delete(int id);
+        List<Tag> GetAll();
+        List<Tag> Find(Expression<Func<Game, bool>> filter);
+    }
+}
---
+++ GameHive.Core/Services/TagService.cs
@@ -0,0 +1,47 @@
+﻿using GameHive.Core.IServices;
+using GameHive.DataAccess.Repository;
+using GameHive.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHive.Core.Services
+{
+    internal class TagService : ITagService
+
+
+        private readonly IRepository<Tag> _repo;
+        public void Add(Tag tag)
+        {
+            
+        }
+
+        public void Delete(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tag> Find(Expression<Func<Game, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tag> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Game GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Game game)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
