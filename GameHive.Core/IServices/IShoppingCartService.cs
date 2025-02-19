e12ba3b3b50461a57693a050fc41760dea69a220
Pashova <ppashova06@gmail.com>
2025-02-19 15:22:17 +0200

    Started work on the Shopping cart and orders

---
+++ GameHive.Core/IServices/IShoppingCartService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GameHive.Models;
+
+namespace GameHive.Core.IServices
+{
+    public interface IShoppingCartService
+    {
+        string GetCartId();
+        Task AddToCartAsync(Game game);
+        Task RemoveFromCartAsync(int gameId);
+        Task<List<Cart>> GetCartItemsAsync();
+        Task EmptyCartAsync();
+    }
+}
---
+++ GameHive.DataAccess/Repository/Repositories/ShoppingCartRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameHive.DataAccess.Repository.IRepositories;
+using GameHive.Models;
+
+namespace GameHive.DataAccess.Repository.Repositories
+{
+    public class ShoppingCartRepository : IShoppingCartRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public async Task AddToCartAsync(Cart cartItem)
+        {
+            _context.Carts.Add(cartItem);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EmptyCartAsync(string CartId)
+        {
+            var cartItems = _context.Carts.Where(c => c.CartId == CartId);
+            _context.Carts.RemoveRange(cartItems);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Cart> GetCartItemAsync(string cartId, int gameId)
+        {
+            return await _context.Carts.FirstOrDefaultAsync(c => c.CartId == cartId && c.GameId == gameId);
+        }
+
+        public async Task<List<Cart>> GetCartItemsAsync(string cartId)
+        {
+            return await _context.Carts.Where(c => c.CartId == cartId).Include(c => c.Game).ToListAsync();
+        }
+
+        public async Task RemoveFromCartAsync(Cart cartItem)
+        {
+            _context.Carts.Remove(cartItem);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateCartAsync(Cart cartItem)
+        {
+            _context.Carts.Update(cartItem);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
