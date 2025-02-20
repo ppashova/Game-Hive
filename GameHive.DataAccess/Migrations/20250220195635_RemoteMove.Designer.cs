cdb7283b48e1c55799691901e192ab549901e6bd
ppashova <ppashova06@gmail.com>
2025-02-20 22:37:14 +0200

    Fixed a problem with showing the total price in the Shopping cart

--- GameHive.DataAccess/Migrations/20250220195635_RemoteMove.Designer.cs
+++ GameHive.DataAccess/Migrations/20250220195635_RemoteMove.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GameHive.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250220121142_Create")]
-    partial class Create
+    [Migration("20250220195635_RemoteMove")]
+    partial class RemoteMove
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- GameHive.DataAccess/Migrations/20250220195635_RemoteMove.cs
+++ GameHive.DataAccess/Migrations/20250220195635_RemoteMove.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace GameHive.DataAccess.Migrations
 {
     /// <inheritdoc />
-    public partial class Create : Migration
+    public partial class RemoteMove : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
