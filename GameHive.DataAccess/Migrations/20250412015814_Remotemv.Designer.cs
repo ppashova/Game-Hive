5e88d4db50e370dc575452951664c738df6f8ad5
ppashova <ppashova06@gmail.com>
2025-04-12 05:23:11 +0300

    Fixed issues with the user profile managment

--- GameHive.DataAccess/Migrations/20250412015814_Remotemv.Designer.cs
+++ GameHive.DataAccess/Migrations/20250412015814_Remotemv.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GameHive.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250409063141_RemoteMv")]
-    partial class RemoteMv
+    [Migration("20250412015814_Remotemv")]
+    partial class Remotemv
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
